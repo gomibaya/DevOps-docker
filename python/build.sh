db7884f1bdbc37b07a6535e5fc657558fd84944d
Esteban Barón <esteban@gominet.net>
2021-09-06 02:40:34 +0200

    Actualizado a buster 10.10 Agregado plantilla de imagen python

--- python/build.sh
+++ python/build.sh
@@ -25,24 +25,19 @@ localversion=$(cat version.txt)
 
 for rawversion in ${versions}; do
     for arch in "${archs}"; do
-	echo "Arch: $arch"
         rawversion=$(echo $rawversion | awk -v ARCH=$arch -F'|' '{gsub(/%ARCH%/,ARCH); print $0}')
         version=$(echo $rawversion | awk -F'|' '{print $1}')
         fullversion=$(wget -qO- $(echo $rawversion | awk -F'|' '{print $2}'))
         dir=$(echo $rawversion | awk -v ARCH=$arch -v FULLVERSION=$fullversion -F'|' '{printf "%s/%s/%s", $1, ARCH, FULLVERSION}')
-	tag=$(echo $rawversion | awk -v ARCH=$arch -v FULLVERSION=$fullversion -F'|' '{printf "%s-%s-%s", ARCH, $1, FULLVERSION}')
-	echo "Dir: $dir"
         dockerfile=$dir/Dockerfile
         if [ ! -f $dockerfile ]; then
             echo "${version}:${arch} ERROR No previus dockerfile exists."
             continue
         fi
         #${LOCALUSERNAME}/$IMAGE:$VERSION
-	echo "Lanzo: docker build -f $dockerfile -t ${DOCKERUSERNAME}/devops-base:${tag} $dir"
-        docker build -f $dockerfile --no-cache -t ${DOCKERUSERNAME}/devops-base:${tag} $dir
+        docker build -f $dockerfile -t ${DOCKERUSERNAME}/devops-base:${version} $dir
         cd $dir
-	echo "Lanzo: trivy image --severity HIGH,CRITICAL ${DOCKERUSERNAME}/devops-base:${tag}"
-        trivy image --severity HIGH,CRITICAL ${DOCKERUSERNAME}/devops-base:${tag}
+        trivy image --severity HIGH,CRITICAL ${DOCKERUSERNAME}/devops-base:${version}
         cd $oldpwd
         
     done
